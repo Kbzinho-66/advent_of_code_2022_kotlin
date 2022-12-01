473ac47088ebdb00ade8d700caf36621a016d39b
Kbzinho-66 <gvalles@ucs.br>
2022-12-01 19:54:14 -0300

    Partes 1 e 2 do 1º dia

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
+import java.lang.IllegalStateException
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(calories: List<Int>): Int {
+        return calories[0]
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(calories: List<Int>): Int {
+        return calories.slice(0..2).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
+    fun getCalories(input: List<String>): List<Int> {
+        var carried = 0
+        val caloriesPerElf: MutableList<Int> = mutableListOf<Int>()
+
+        val it: ListIterator<String> = input.listIterator()
+        while (it.hasNext()) {
+            val cal = it.next()
+            if (cal.isNotEmpty()) carried += cal.toInt()
+
+            // Esses ifs tão separados pra pegar o último número do arquivo
+            if (cal.isEmpty() || !it.hasNext()) {
+                caloriesPerElf.add(carried)
+                carried = 0
+            }
+        }
+
+        return caloriesPerElf.sortedDescending()
+    }
+
+    // Testar os casos básicos
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testCalories = getCalories(testInput)
+    val test1 = part1(testCalories)
+    val test2 = part2(testCalories)
+    if (test1 != 24000) throw IllegalStateException("$test1 != 24000")
+    if (test2 != 45000) throw IllegalStateException("$test2 != 45000")
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val caloriesPerElf = getCalories(input)
+    println("Parte 1 = ${part1(caloriesPerElf)}")
+    println("Parte 2 = ${part2(caloriesPerElf)}")
 }
